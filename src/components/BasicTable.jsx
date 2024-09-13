eaf025182cae93f2aae57d37da2704b787d4f9be
1Papillon1 <83593078+1Papillon1@users.noreply.github.com>
2024-09-13 14:35:45 +0200

    dodavanje podataka u localStorage.

---
+++ src/components/BasicTable.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Table from '@mui/joy/Table';
+
+
+export default function BasicTable() 
+{
+
+    return (
+        <h1>Main</h1>
+    )
+}
--- src/routes/ExpensesPath.jsx
+++ src/routes/ExpensesPath.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Card from "../components/Card";
-import Table from "../components/Table";
+import Table from "../components/BasicTable";
 
 export default function ExpensesPath() {
 
